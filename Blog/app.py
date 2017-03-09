0a13d1b78191d8811fe10956bad240bd8692731f
mixassio <mixail.bondarev@gmail.com>
2017-03-09 21:42:05 +0300

    Доделал задание по блогу

--- Blog/app.py
+++ Blog/app.py
@@ -1,26 +1,42 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 app.config.from_object(config)
 db = SQLAlchemy(app=app)
 
-#Здесь пока нет логики, идет проверка на добавление записей в базу
-#Всё загружается, но сервер выдаёт какую-то ошибку, не понял пока
-@app.route('/')
-def posts():
-    from models import Post, User, Tag, Tags
-    user = User(name='ivan', email='ivan@mail.ru')
-    post = Post(title='kak ya provel etim letom', content='ochen horosho provel', is_visible=True, user_id=1)
-    tag = Tag(title='kitchen')
-    postteg = Tags(tag_id=2, post_id=2)
-    print(post.title, post.slug)
-    db.session.add(tag)
-    db.session.add(user)
-    db.session.add(post)
-    db.session.add(postteg)
+
+@app.route('/create/<tag>')
+def create_tag(tag):
+    from models import Tag
+    print(tag)
+    new_tag = Tag(title=tag)
+    db.session.add(new_tag)
     db.session.commit()
+    return 'done'
+
+
+@app.route('/create/post/', methods=['GET', 'POST'])
+def create_post():
+    from models import Post
+    from forms import PostForm
+
+    if request.method == 'POST':
+        form = PostForm(request.form)
+    if form.validate():
+        post = Post(**form.data)
+        db.session.add(post)
+        db.session.commit()
+    return 'done'
+
+
+@app.route('/all_post')
+def all_post():
+    sel = db.session.query(Post).all()
+    return render_template('all_post.txt', posts=sel)
 
 if __name__ == '__main__':
     from models import db, Post, User, Tag, Tags
